4d8b980387ac54abee1226cc243a036961e383af
Maik Kempe <mkempe@bitaculous.com>
2022-11-09 21:09:02 +0100

    [♻] Cleaned up.

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,13 +6,3 @@ declare(strict_types=1);
  */
 
 defined('TYPO3') or die();
-
-// (static function () {
-//     // === Variables ===
-//
-//     // $namespace = 'T3v';
-//     // $extensionKey = 't3v_translations';
-//     // $extensionSignature = ExtensionUtility::getSignature($namespace, $extensionKey);
-//
-//     // === T3v Generator ===
-// })();
